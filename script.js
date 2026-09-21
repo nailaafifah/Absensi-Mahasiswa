4529c9ca8e5ef7051e936f3f664e58f1034be0f5
nailaafifah <o66830504@gmail.com>
2026-09-21 13:48:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
     loadDashboard();
 
     document.getElementById("npm").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            cekMahasiswa();
         }
     });
 });
